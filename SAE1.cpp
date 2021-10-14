df973a662d89a681c3ed6395b2c94c8e9926c80f
Theo boursy <hyptonus@gmail.com>
2021-10-14 22:38:15 +0200

    Create SAE1.cpp
    Co-Authored-By: AlJZ <46426662+AlJZ@users.noreply.github.com>

---
+++ SAE1.cpp
@@ -0,0 +1,76 @@
+#include<stdio.h> 
+#include<iostream>          
+#include <string> // importation des bibliothèques neccessaires
+using namespace std;
+
+void parametre(int *nb_allumette,char joueur[],char *ordi,char start[])//fonction pour le parametrage de la partie
+{
+    cout<<"entrez votre pseudo"<<endl;
+    cin>>joueur;
+    cout<<"Voulez vous commencer? marquez oui, sinon, marquez non"<<endl;
+    cin>>start;
+    if(start != "Oui" & start != "Non") //verification de la valeur start, valable ou non
+        {
+            while (start != "Oui" && start != "Non")// tant que la valeur n'est pas bonne, on redemande de choisir qui commence
+            {
+                cout<<"Je n'ai pas compris, veuillez reesayer"<<endl;
+                cin>>start;
+            }
+        }
+    if (start == "oui")
+    {
+        start = "joueur";
+    }
+    else
+    {
+        start = "PC";
+    }
+    cout << "entrez le nombre d'allumette souhaté entre 1 et 30" << endl;
+    cin >> *nb_allumette;
+    while (*nb_allumette < 1 || *nb_allumette > 30)  
+    {
+        cin >> *nb_allumette;
+        if (!cin >> *nb_allumette || *nb_allumette < 1 || *nb_allumette > 30)
+        {
+            cout << "Valeur incorrect, choisissez un nombre entre 1 et 30 " << endl;
+            cin.clear(); 
+            cin.ignore(100, '\n');
+        }
+    } 
+    
+    cout << "entrez le niveau de l'ordinateur avec N pour naif ou E pour expert" << endl;
+    cin>>*ordi;
+    if (*ordi!='e' && *ordi!='E' && *ordi !='n' && *ordi !='N')
+    {
+        while(*ordi!= 'e' && *ordi!= 'E' && *ordi !='n' && *ordi != 'N')
+        {
+            cout<<"Valeur incorrect, merci de choisir entre E et N"<<endl;
+            cin>>*ordi;
+        }
+    }
+    
+}
+void affiche_Allumettes(int *nb_allumettes)
+{
+    char allumettes = '!';
+    for(int i = 0; i < *nb_allumettes; i++)
+    {
+        cout<<allumettes;
+        if(i%5 == 0)
+        {
+            cout<<endl;
+        }
+    }
+}
+
+int main()
+{
+
+    int nb_allumette, nb_allumette_rest;
+    char joueur[50], start[10]; 
+    char ordi;
+    // parametre(&nb_allumette,joueur,&ordi,start);
+    // cout<<"nombre allumettes: "<<nb_allumette<<" pseudo: "<<joueur<<" difficulte: "<<ordi<<endl;
+    affiche_Allumettes
+    return 0;
+}
